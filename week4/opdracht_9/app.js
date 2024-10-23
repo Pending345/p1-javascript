1b3a356e779d98c9ec6d17a293e5994cc952d3cb
Pending345 <rafhildert@gmail.com>
2024-10-23 15:48:15 +0200

    opdracht_9

---
+++ week4/opdracht_9/app.js
@@ -0,0 +1,15 @@
+let button = document.getElementById('button');
+let count = document.getElementById('count');
+
+let c = 0;
+
+count.innerText = `The button was clicked ${c} times!`;
+
+button.addEventListener('click', () => {
+    c++;
+    count.innerText = `The button was clicked ${c} times!`;
+
+    if (c > 9) {
+       c = -1
+    }
+});
