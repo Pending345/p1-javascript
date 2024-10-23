1fed3af64d27ec3494ddd8bb140f09f5c06ac541
Pending345 <rafhildert@gmail.com>
2024-10-23 16:11:04 +0200

    opdracht_6

---
+++ week3/app.js
@@ -0,0 +1,17 @@
+function calculate(a, b) {
+  return a + b;
+}
+
+function multiplyByFive(a) {
+  return a * 5;
+}
+
+function calculateMinutes(a) {
+  return a / 60;
+}
+
+console.log(calculate(1,12));
+
+console.log(multiplyByFive(7));
+
+console.log(calculateMinutes(3600));
